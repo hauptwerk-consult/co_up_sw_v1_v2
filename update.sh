1c4c92ff35a86922c5af00e3aaa7b082227aaf75
Gert Jan Schipper <gert-jan.schipper@visma.com>
2024-06-12 16:07:53 +0200

    chore: Update Sweelinq configuration files and create symbolic links for Suite versions

--- update.sh
+++ update.sh
@@ -35,6 +35,9 @@ print 'INFO: Create content_sudo file'
 exec_command "chown root:root ${SCRIPT_DIR}/user_content_sudo"
 exec_command "cp -rp ${SCRIPT_DIR}/user_content_sudo /etc/sudoers.d"
 
+print 'INFO: Check Suite Version'
+exec_command "SUITE=`ls -al /home/content/Sweelinq/UserData | grep Suite_ | awk '{print $11}' | awk 'BEGIN { FS = "/" } ; { print $5 };'`"
+
 print 'INFO: Move startSweelinq.sh file'
 exec_command "mv ${SCRIPT_DIR}/startSweelinq.sh ${HOME}/.script"
 exec_command "chmod +x ${HOME}/.script/startSweelinq.sh"
@@ -50,6 +53,19 @@ exec_command 'wget https://cosweelinq.blob.core.windows.net/sweelinq/Sweelinq.ta
 
 print 'INFO: Install Sweelinq Version 2'
 exec_command "tar -xf /home/content/Downloads/Sweelinq.tar -C /opt"
+
+print 'INFO: Copy Sweelinq Configuration Files'
+exec_command "cp -rp ${SCRIPT_DIR}/GeneralSettings* ${HOME}/.swlnqcfg"
+
+if [ "$SUITE" == "Suite_II" ]; then
+	print 'INFO: Make symoblic link for Suite_II'
+	exec_command "ln -sf ${HOME}/.swlnqcfg/GeneralSettings-Suite_II.swgs ${HOME}/Sweelinq/UserData/GeneralSettings.swgs"
+fi
+
+if [ "$SUITE" == "Suite_III" ]; then
+	print 'INFO: Make symoblic link for Suite_III'
+	exec_command "ln -sf ${HOME}/.swlnqcfg/GeneralSettings-Suite_III.swgs ${HOME}/Sweelinq/UserData/GeneralSettings.swgs"
+fi
 
 print 'INFO: Remove all files in Download folder'
 exec_command 'rm -rf /home/content/Downloads/*'
