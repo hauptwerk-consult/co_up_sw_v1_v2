f8b6a88e9e324b1277634d36fb0fa253012b8dca
Gert Jan Schipper <info@hauptwerkconsult.com>
2024-06-12 20:37:45 +0200

    append to logfile

--- update.sh
+++ update.sh
@@ -14,7 +14,7 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 PW=`cat ${HOME}/.pw`
 
 exec_command () {
-	script -c "echo ${PW} | sudo -S $1" ${LOGFILE}
+	script -ac "echo ${PW} | sudo -S $1" ${LOGFILE}
 }
 
 print 'INFO: Update Linux libraries'
