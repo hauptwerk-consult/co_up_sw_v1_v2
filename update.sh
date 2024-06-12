2c7a3befe0c4477e1521d536dfd3682d26f44934
Gert Jan Schipper <info@hauptwerkconsult.com>
2024-06-12 09:18:29 +0200

    copy sddm.conf file

--- update.sh
+++ update.sh
@@ -15,7 +15,15 @@ exec_command () {
 	echo ${PW} | sudo -S $1 | tee -a ${LOGFILE} 2>&1
 }
 
+print 'INFO: Update Linux libraries'
+exec_command 'pacman -Syu'
+
+print 'INFO: Install konsole package'
 exec_command 'pacman -S konsole'
-exec_command 'pacman -S wget'
+
+print 'INFO: Settings for autologin'
+exec_command 'chown root:root ${SCRIPT_DIR}/sddm.conf'
+exec_command 'cp -rp ${SCRIPT_DIR}/sddm.conf /etc'
+
 print 'INFO: Sleep for 20 seconds ...'
 exec_command 'sleep 20'
