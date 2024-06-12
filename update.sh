884255f7beffb957e6117c1b6dd8932faeb8f985
Gert Jan Schipper <info@hauptwerkconsult.com>
2024-06-12 20:30:50 +0200

    check if sweelinq v2 is installed skip it

--- update.sh
+++ update.sh
@@ -9,7 +9,7 @@ print () {
 	sleep 3
 }
 
-print "INFO: Initialize for Content ${SUITE}
+print "INFO: Initialize for Content ${SUITE}"
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 PW=`cat ${HOME}/.pw`
 
@@ -18,58 +18,67 @@ exec_command () {
 }
 
 print 'INFO: Update Linux libraries'
-exec_command 'pacman -Syu --noconfirm'
+exec_command "pacman -Syu --noconfirm"
 
 print 'INFO: Install konsole package'
-exec_command 'pacman -S konsole --noconfirm'
+exec_command "pacman -S konsole --noconfirm"
 
 print 'INFO: Settings for autologin'
 exec_command "chown root:root ${SCRIPT_DIR}/sddm.conf"
 exec_command "cp -rp ${SCRIPT_DIR}/sddm.conf /etc"
 
 print 'INFO: Install webkit2gtk'
-exec_command 'pacman -S webkit2gtk --noconfirm'
+exec_command "pacman -S webkit2gtk --noconfirm"
 
 print 'INFO: Install tk'
-exec_command 'pacman -S tk --noconfirm'
+exec_command "pacman -S tk --noconfirm"
 
 print 'INFO: Create content_sudo file'
 exec_command "chown root:root ${SCRIPT_DIR}/user_content_sudo"
 exec_command "cp -rp ${SCRIPT_DIR}/user_content_sudo /etc/sudoers.d"
 
 print 'INFO: Move startSweelinq.sh file'
-exec_command "mv ${SCRIPT_DIR}/startSweelinq.sh ${HOME}/.script"
+exec_command "mv -f ${SCRIPT_DIR}/startSweelinq.sh ${HOME}/.script"
 exec_command "chmod +x ${HOME}/.script/startSweelinq.sh"
 
 print 'INFO: Move sweelinq_delay.py file'
-exec_command "mv ${SCRIPT_DIR}/sweelinq_delay.py ${HOME}/.script"
+exec_command "mv -f ${SCRIPT_DIR}/sweelinq_delay.py ${HOME}/.script"
 
-print 'INFO: Move current Sweelinq v1 version'
-exec_command "mv /opt/Sweelinq /opt/Sweelinq_v1_${TIME}"
+if [ ! -d /opt/Sweelinq_v1* ]
+then 
+	print 'INFO: Move current Sweelinq v1 version'
+	exec_command "mv -f /opt/Sweelinq /opt/Sweelinq_v1_${TIME}"
+fi
 
-print 'INFO: Download Sweelinq binaries'
-exec_command 'wget https://cosweelinq.blob.core.windows.net/sweelinq/Sweelinq.tar'
+if [ ! -d /opt/Sweelinq_v1* ]
+then
+	print 'INFO: Download Sweelinq binaries'
+ 	exec_command "wget https://cosweelinq.blob.core.windows.net/sweelinq/Sweelinq.tar"
+fi
 
-print 'INFO: Install Sweelinq Version 2'
-exec_command "tar -xf /home/content/Downloads/Sweelinq.tar -C /opt"
+if [ ! -d /opt/Sweelinq_v1* ]
+then
+	print 'INFO: Install Sweelinq Version 2'
+	exec_command "tar -xf /home/content/Downloads/Sweelinq.tar -C /opt"
+fi
 
 print 'INFO: Copy Sweelinq Configuration Files'
 exec_command "cp -rp ${SCRIPT_DIR}/GeneralSettings* ${HOME}/.swlnqcfg"
 
-if [ "$SUITE" == "Suite_II" ]; then
+if [ "${SUITE}" == "Suite_II" ]; then
 	print 'INFO: Make symoblic link for Suite_II'
 	exec_command "ln -sf ${HOME}/.swlnqcfg/GeneralSettings-Suite_II.swgs ${HOME}/Sweelinq/UserData/GeneralSettings.swgs"
 fi
 
-if [ "$SUITE" == "Suite_III" ]; then
+if [ "${SUITE}" == "Suite_III" ]; then
 	print 'INFO: Make symoblic link for Suite_III'
 	exec_command "ln -sf ${HOME}/.swlnqcfg/GeneralSettings-Suite_III.swgs ${HOME}/Sweelinq/UserData/GeneralSettings.swgs"
 fi
 
 print 'INFO: Install extra Packages'
-exec_command 'pacman -S acpid --noconfirm'
-exec_command 'systemctl start acpid'
-exec_command 'systemctl enable acpid'
+exec_command "pacman -S acpid --noconfirm"
+exec_command "systemctl start acpid"
+exec_command "systemctl enable acpid"
 
 print 'INFO: Create power file'
 exec_command "sudo mkdir -p /etc/acpi/events"
@@ -80,11 +89,11 @@ exec_command "chown root:root snd-virmidi.conf"
 exec_command "cp -rp ${SCRIPT_DIR}/snd-virmidi.conf /etc/modules-load.d"
 
 print 'INFO: Remove all files in Downloads folder'
-exec_command 'rm -rf /home/content/Downloads/*'
+exec_command "rm -rf /home/content/Downloads/*"
 
 print 'INFO: Remove old Organ files'
 exec_command "rm -rf ${HOME}/Sweelinq/Organs/*.swop"
 exec_command "rm -rf ${HOME}/Sweelinq/Organs/*.bin"
 
 print 'INFO: Restart System'
-exec_command 'shutdown -r -t0 now'
+#exec_command "shutdown -r -t0 now"
